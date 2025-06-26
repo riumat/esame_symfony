4331f2eb38a0a247f500ae94295fbebe84ee6b5d
riumat <matteoariu42@gmail.com>
2025-06-26 10:18:27 +0200

    logout

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { removeToken } from "../../libs/storage";
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    removeToken();
+    navigate("/login", { replace: true });
+  };
   return (
     <nav className=" bg-gradient-to-l from-chart-5/20 to-chart-1/20 shadow-md sticky top-0 z-50 backdrop-blur-xl">
       <ul className="ml-20 gap-10 flex justify-start items-center p-4 text-base  text-black">
@@ -17,6 +23,14 @@ const Navbar: React.FC = () => {
           <Link className="hover:underline duration-300 px-5 py-2" to="/user">
             La mia lista
           </Link>
+        </li>
+        <li>
+          <button
+            className="hover:underline duration-300 px-5 py-2"
+            onClick={handleLogout}
+          >
+            Esci
+          </button>
         </li>
       </ul>
     </nav>
